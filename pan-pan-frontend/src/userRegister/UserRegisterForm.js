151b7a343f9d1e67ac6cdb6a1a91118003faed07
Josue Nascimento <josuetk63@gmail.com>
2018-04-05 14:10:21 -0300

    Change data user

--- pan-pan-frontend/src/userRegister/UserRegisterForm.js
+++ pan-pan-frontend/src/userRegister/UserRegisterForm.js
@@ -1,42 +1,46 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 import './UserRegisterForm.css'
 
 class UserRegisterForm extends Component{
   constructor(props){
     super(props);
       this.state = {
-          name: '',
-          email:'',
-          password:'',
-          instrument:'',
-          city:'',
-          state:''
+          firstName: "",
+          lastName:"",
+          birthdate:"",
+          city:"",
+          state:"",
+          instrument:[],
+          email:"",
+          username:"",
+          password:"",
+          role:"",
+          active:""
       }
+
   }
 
-  handleNameChange(event){
-        this.setState({name: event.target.value});
-  }
-  handleCityChange(event){
-    this.setState({city: event.target.value});
-  }
-  handleStateChange(event){
-    this.setState({State: event.target.value});
-  }
-  handleEmailChange(event){
-    this.setState({email: event.target.value});
-  }
-  handleInstrumentChange(event){
-    this.setState({instrument: event.target.value});
-  }
-  handlePasswordChange(event){
-    this.setState({password: event.target.value});
+  submitForm(event){
+    event.preventDefault();
+    const data_form = new FormData(event.target);
+
+    const data_user = {
+          name: data_form.get('name'),
+          email: data_form.get('email'),
+          birthday: data_form.get('birthday'),
+          password: data_form.get('password'),
+          instrument: data_form.get('instrument'),
+          city: data_form.get('city'),
+          state: data_form.get('state'),
+          username: data_form.get('username')
+    }
+
+    axios.post('', data_user)
+      .then(response => console.log(response.data))
+
   }
 
-  listState(){
-    
-  }
   render(){
     var html =
           <body className="user-register-back">
@@ -48,55 +52,55 @@ class UserRegisterForm extends Component{
                   <font id="pan-pan-header" face="Norwester"><b>Pan-Pan</b></font>
                 </header>
                     <div class="form-group">
-                    <form id="registerForm" onSubmit={this.userInput}>
-
+                    <form id="registerForm" onSubmit={this.submitForm}>
+                      <label id="label-register-title"><font face= "Norwester"><b>Cadastro</b> </font></label>
                       <div id="label-format">
                         <label class="label-text-font"><font face="Norwester"><b>Nome:</b></font></label>
                       </div>
                       <div id="input-text-format">
-                          <input type="text" class="form-control col-lg-4" required="required "placeholder="Seu nome" name="name" value={this.state.name}  onChange={(event) => this.handleNameChange(event)}></input>
+                          <input type="text" class="form-control col-lg-4" required="required "placeholder="Seu nome" name="name"  ></input>
                         </div>
                       <div id="label-format-camp">
                         <label  class="label-text-font" ><font face="Norwester" ><b>E-mail:</b></font></label>
                       </div>
                       <div id="input-text-format">
-                        <input type="text" class= "form-control col-lg-4" placeholder="Seu email" required="required" name="email" onChange={(event) => this.handleEmailChange(event)}></input>
+                        <input type="text" class= "form-control col-lg-4" placeholder="Seu email" required="required" name="email"></input>
                       </div>
                       <div id="label-format-age">
                         <label  class="label-text-font" ><font face="Norwester" ><b>Data de Nascimento</b></font></label>
                       </div>
                       <div id="input-text-format">
-                        <input type="text" class= "form-control col-lg-4" placeholder="**/**/****" required="required" name="age" onChange={(event) => this.handleEmailChange(event)}></input>
+                        <input type="text" class= "form-control col-lg-4" placeholder="**/**/****" required="required" name="birthday"></input>
                       </div>
                       <div id="label-format-camp-instrument">
                         <label class="label-text-font"><font face="Norwester"><b>Instrumetos:</b></font></label>
                       </div>
                       <div id="input-text-format">
-                        <input type="text" class= "form-control col-lg-4" placeholder="Qual seu Instrumento?" required="required" name="instrument" onChange={(event) => this.handleInstrumentChange(event)}></input>
+                        <input type="text" class= "form-control col-lg-4" placeholder="Qual seu Instrumento?" required="required" name="instrument" ></input>
                       </div>
                       <div id="label-format-state-instrument">
                         <label class="label-text-font"><font face="Norwester"><b>Estado:</b></font></label>
                       </div>
                       <div id="input-text-format">
-                        <input type="text" class= "form-control col-lg-4" placeholder="Qual sua estado?" required="required" name="state" onChange={(event) => this.handleStateChange(event)}></input>
+                        <input type="text" class= "form-control col-lg-4" placeholder="Qual sua estado?" required="required" name="state"></input>
                       </div>
                       <div id="label-format-camp-city">
                         <label class="label-text-font"><font face="Norwester"><b>Cidade:</b></font></label>
                       </div>
                       <div id="input-text-format">
-                        <input type="text" class= "form-control col-lg-4" placeholder="Qual seu cidade?" required="required" name="city" onChange={(event) => this.handleCityChange(event)}></input>
+                        <input type="text" class= "form-control col-lg-4" placeholder="Qual seu cidade?" required="required" name="city"></input>
                       </div>
                       <div id="label-format-camp-username">
                         <label class="label-text-font"><font face="Norwester"><b>UserName:</b></font></label>
                       </div>
                       <div id="input-text-format">
-                        <input type="password" class= "form-control col-lg-4" placeholder="Insira um apelido" required="required" name="username" onChange={(event) => this.handlePasswordChange(event)}></input>
+                        <input type="text" class= "form-control col-lg-4" placeholder="Insira um apelido" required="required" name="username"></input>
                       </div>
                       <div id="label-format-camp">
                         <label class="label-text-font"><font face="Norwester"><b>Senha:</b></font></label>
                       </div>
                       <div id="input-text-format">
-                        <input type="password" class= "form-control col-lg-4" placeholder="Insira uma senha" required="required" name="password" onChange={(event) => this.handlePasswordChange(event)}></input>
+                        <input type="password" class= "form-control col-lg-4" placeholder="Insira uma senha" required="required" name="password"></input>
                       </div>
 
                       <div id="label-format-repite-password">
