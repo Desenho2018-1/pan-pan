9cc891040f382e58f0f6dcb8a4f28d426203a1c6
shammyz <rodrigo.redcode@gmail.com>
2018-04-05 15:42:51 -0300

    Add jpa repository for verification token queries

---
+++ pan-pan-backend/src/main/java/com/desenho/panpan/repository/VerificationTokenJpaRepository.java
@@ -0,0 +1,12 @@
+package com.desenho.panpan.repository;
+
+import com.desenho.panpan.model.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public interface VerificationTokenJpaRepository extends JpaRepository<VerificationToken, Long>{
+	
+	VerificationToken findByToken(String token);
+}
