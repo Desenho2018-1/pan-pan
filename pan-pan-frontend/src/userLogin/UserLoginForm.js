67f93a6842e6739d33301dc269246f60f67680a4
Josue Nascimento <josuetk63@gmail.com>
2018-04-07 13:35:36 -0300

    Create login user page

--- pan-pan-frontend/src/userLogin/UserLoginForm.js
+++ pan-pan-frontend/src/userLogin/UserLoginForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import '../base/base.css';
-
+import './UserLoginForm.css'
 
 class UserLoginForm extends Component{
   constructor(props){
@@ -15,9 +15,27 @@ class UserLoginForm extends Component{
         var html =
             <body className="base-body">
                 <header className="base-header">
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"></link>
+                  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"></link>
                 </header>
-                <div class="form-group"
+                <div id="title-login">
+                  <label id="title-login-label" ><b><font face="Norwester" >Login</font></b></label>
+                </div>
+                <div class="form-group">
+                 <form id="user-login-form" onSubmit="">
+                  <div id="label-format-camp">
+                    <label  class="label-text-font" ><font face="Norwester" ><b>E-mail:</b></font></label>
+                  </div>
+                  <div id="input-text-format">
+                    <input type="text" class= "form-control col-lg-4" placeholder="Seu email" required="required" name="email"></input>
+                  </div>
+                  <div id="label-format-camp">
+                    <label class="label-text-font"><font face="Norwester"><b>Senha:</b></font></label>
+                  </div>
+                  <div id="input-text-format">
+                    <input type="password" class= "form-control col-lg-4" placeholder="Insira uma senha" required="required" name="password"></input>
+                  </div>
+                      <button id="button-register" type="submit" class="btn btn-secondary btn-lg" value="register">Entrar</button>
+                  </form>
                 </div>
             </body>
         return html;
