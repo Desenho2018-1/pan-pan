cac83cfd7cba28392cc88851b3322f995bb82c5b
Roger Lenke <rogerlenke@gmail.com>
2018-04-09 09:37:42 -0300

    Fixing UserLoginForm code style.

--- pan-pan-frontend/src/userLogin/UserLoginForm.js
+++ pan-pan-frontend/src/userLogin/UserLoginForm.js
@@ -1,60 +1,67 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react';
 import '../base/base.css';
-import './UserLoginForm.css'
-import HeaderPage from '../headerPage/HeaderPage.js'
-import LabelPage from '../labelsPage/LabelPage.js'
+import './UserLoginForm.css';
+import HeaderPage from '../headerPage/HeaderPage';
+import LabelPage from '../labelsPage/LabelPage';
 
-class UserLoginForm extends Component{
-  constructor(props){
+class UserLoginForm extends Component {
+  constructor(props) {
     super(props);
-      this.state = {
-          email: '',
-          password: ''
-      };
-      this.handleEmail = this.handleEmail.bind(this);
-      this.handlePassword = this.handlePassword.bind(this);
-      this.loginUser = this.loginUser.bind(this);
-    }
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleEmail = this.handleEmail.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
+    this.loginUser = this.loginUser.bind(this);
+  }
 
-    loginUser(event){
-      event.preventDefault();
-      console.log(this.state.email);
-      console.log(this.state.password);
-    }
+  loginUser(event) {
+    event.preventDefault();
+    console.log(this.state.email);
+    console.log(this.state.password);
+  }
 
-    handleEmail(event){
-       this.setState({email: event.target.value});
-    }
-    handlePassword(event){
-       this.setState({password: event.target.value});
-    }
-    render(){
-        var html =
-            <body className="base-body">
-                <HeaderPage userName='josue'/>
-                <div id="title-login">
-                  <label id="title-login-label" ><b><font face="Norwester" >Login</font></b></label>
-                </div>
-                <div class="form-group">
-                 <form id="user-login-form" onSubmit={this.loginUser}>
-                  <div id="label-format-camp">
-                    <LabelPage label='E-mail:'/>
-                  </div>
-                  <div id="text-input">
-                    <input type="text" class= "form-control col-lg-4" onChange={this.handleEmail} value={this.state.email} placeholder="Seu email" required="required" name="email"></input>
-                  </div>
-                  <div id="label-format-camp">
-                    <LabelPage label='Senha:'/>
-                  </div>
-                  <div id="text-input">
-                    <input type="password" id="" class= "form-control col-lg-4" onChange={this.handlePassword} value={this.state.password} placeholder="Insira uma senha" required="required" name="password"></input>
-                  </div>
-                      <button id="button-register" type="submit" class="btn btn-secondary btn-lg" value="register">Entrar</button>
-                  </form>
-                </div>
-            </body>
-        return html;
-    }
+  handleEmail(event) {
+    this.setState({ email: event.target.value });
+  }
+  handlePassword(event) {
+    this.setState({ password: event.target.value });
+  }
+  render() {
+    return (
+      <body className="base-body">
+        <HeaderPage userName="josue" />
+        <div id="title-login">
+          <label htmlFor="name" id="title-login-label" ><b><font face="Norwester" >Login</font></b></label>
+        </div>
+        <div className="form-group">
+          <form id="user-login-form" onSubmit={this.loginUser}>
+            <div id="label-format-camp">
+              <LabelPage label="E-mail:" />
+            </div>
+            <div id="text-input">
+              <input
+                type="text"
+                className="form-control col-lg-4"
+                onChange={this.handleEmail}
+                value={this.state.email}
+                placeholder="Seu email"
+                required="required"
+                name="email"
+              />
+            </div>
+            <div id="label-format-camp">
+              <LabelPage label="Senha:" />
+            </div>
+            <div id="text-input">
+              <input type="password" id="" className="form-control col-lg-4" onChange={this.handlePassword} value={this.state.password} placeholder="Insira uma senha" required="required" name="password" />
+            </div>
+            <button id="button-register" type="submit" className="btn btn-secondary btn-lg" value="register">Entrar</button>
+          </form>
+        </div>
+      </body>
+    );
+  }
 }
-export default UserLoginForm
+export default UserLoginForm;
