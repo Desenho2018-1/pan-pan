3182c88dc26948f01bf9ddb84fc99d4eab7c1bd8
shammyz <rodrigo.redcode@gmail.com>
2018-04-18 17:52:04 -0300

    Add submit form handler and component constructor

--- pan-pan-frontend/src/bandCreation/BandCreationForm.jsx
+++ pan-pan-frontend/src/bandCreation/BandCreationForm.jsx
@@ -7,21 +7,47 @@ import LabelPage from '../labelsPage/LabelPage';
 import axios from "axios";
 
 export default class BandCreationForm extends PureComponent {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            bandName: "",
+            bandStyle: "",
+            bandImagePath: "",
+            ownerId: "",
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        const bandData = {
+            bandName: formData.get("band-name"),
+            bandStyle: formData.get("band-style"),
+            bandImagePath: "",
+            ownerId: "",
+        }
+        alert("Banda " + bandData.bandName + " cadastrada!")
+    }
+
     render() {
         return (
         <div>
-            <form id="band-form" onSubmit={this.submitForm}>
+            <form id="band-form" onSubmit={this.handleSubmit}>
             <div id="label-format-band-name">
                 <LabelPage label="Nome:" />
             </div>
             <div id="input-text-format">
-                <input type="text" required="required" placeholder="Sua banda" name="band-name"></input>
+                <input type="text" required="required" placeholder="Sua banda" name="band-name"/>
             </div>
             <div id="label-format-band-style">
                 <LabelPage label="Estilo:" />
             </div>
             <div id="input-text-format">
-                <input type="text" required="required" placeholder="Qual estilo musical?" name="band-style"></input>
+                <input type="text" required="required" placeholder="Qual estilo musical?" name="band-style"/>
             </div>
             <button id="select-image-button" type="button">Selecione uma foto</button>
             <button id="submit-button" type="submit" value="register">Cadastrar</button>
