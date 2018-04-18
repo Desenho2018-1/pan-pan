2fbc93e689337b4bda9ee22137d1b0817fecd3ea
shammyz <rodrigo.redcode@gmail.com>
2018-04-18 19:04:21 -0300

    Add initial css for component

--- pan-pan-frontend/src/bandCreation/BandCreationForm.jsx
+++ pan-pan-frontend/src/bandCreation/BandCreationForm.jsx
@@ -1,10 +1,10 @@
 import React, { PureComponent } from 'react'
-import { Link } from 'react-router-dom';
+import LabelPage from '../labelsPage/LabelPage';
+import HeaderPage from '../headerPage/HeaderPage';
+import axios from "axios";
+
 import '../base/base.css';
 import './BandCreationForm.css';
-import HeaderPage from '../headerPage/HeaderPage';
-import LabelPage from '../labelsPage/LabelPage';
-import axios from "axios";
 
 export default class BandCreationForm extends PureComponent {
     constructor(props){
@@ -35,24 +35,29 @@ export default class BandCreationForm extends PureComponent {
 
     render() {
         return (
-        <div>
-            <form id="band-form" onSubmit={this.handleSubmit}>
-            <div id="label-format-band-name">
-                <LabelPage label="Nome:" />
+            <div>
+                <HeaderPage userName='' />
+                <div className="band-creation-form">
+                    <div className="form-group" align="center">
+                        <form id="band-form" onSubmit={this.handleSubmit}>
+                            <div id="label-format-band-name">
+                                <LabelPage label="Nome:" />
+                            </div>
+                            <div id="input-text-format">
+                                <input type="text" required="required" placeholder="Sua banda" name="band-name"/>
+                            </div>
+                            <div id="label-format-band-style">
+                                <LabelPage label="Estilo:" />
+                            </div>
+                            <div id="input-text-format">
+                                <input type="text" required="required" placeholder="Qual estilo musical?" name="band-style"/>
+                            </div>
+                            <button id="select-image-button" type="button" className="btn btn-secondary btn-lg">Selecione uma foto</button>
+                            <button id="submit-button" type="submit" value="submit" className="btn btn-secondary btn-lg">Cadastrar</button>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div id="input-text-format">
-                <input type="text" required="required" placeholder="Sua banda" name="band-name"/>
-            </div>
-            <div id="label-format-band-style">
-                <LabelPage label="Estilo:" />
-            </div>
-            <div id="input-text-format">
-                <input type="text" required="required" placeholder="Qual estilo musical?" name="band-style"/>
-            </div>
-            <button id="select-image-button" type="button">Selecione uma foto</button>
-            <button id="submit-button" type="submit" value="register">Cadastrar</button>
-            </form>
-        </div>
         )
     }
 }
