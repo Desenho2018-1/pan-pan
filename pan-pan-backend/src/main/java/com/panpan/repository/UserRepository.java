3addcd9fd4eef1037d5ca1adb116301e8365853f
pablodiegoss <pablodiegoss@hotmail.com>
2018-04-21 17:18:20 -0300

    Refactor Controllers, bindings and exceptions to new backend

--- pan-pan-backend/src/main/java/com/panpan/repository/UserRepository.java
+++ pan-pan-backend/src/main/java/com/panpan/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.panpan.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.panpan.model.User;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByFirstName(String name);
 
 }
