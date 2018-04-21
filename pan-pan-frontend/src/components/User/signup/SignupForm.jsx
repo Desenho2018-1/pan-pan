d0eb556fcff6dc691ed1e32b548271c0f1366330
Hugo Carvalho <hugonvsc@gmail.com>
2018-04-21 20:56:41 -0300

    Update create user account page

--- pan-pan-frontend/src/components/User/signup/SignupForm.jsx
+++ pan-pan-frontend/src/components/User/signup/SignupForm.jsx
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './SignupForm.css';
+import InputMask from 'react-input-mask';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+
+const STATES = [
+  { label: 'Acre', value: 'AC' },
+  { label: 'Alagoas', value: 'AL' },
+  { label: 'Amapá', value: 'AP' },
+  { label: 'Amazonas', value: 'AM' },
+  { label: 'Bahia', value: 'BA' },
+  { label: 'Ceará', value: 'CE' },
+  { label: 'Distrito Federal', value: 'DF' },
+  { label: 'Espírito Santo', value: 'ES' },
+  { label: 'Goiás', value: 'GO' },
+  { label: 'Maranhão', value: 'MA' },
+  { label: 'Mato Grosso', value: 'MT' },
+  { label: 'Mato Grosso do Sul', value: 'MS' },
+  { label: 'Minas Gerais', value: 'MG' },
+  { label: 'Pará', value: 'PA' },
+  { label: 'Paraíba', value: 'PB' },
+  { label: 'Paraná', value: 'PR' },
+  { label: 'Pernambuco', value: 'PE' },
+  { label: 'Piauí', value: 'PI' },
+  { label: 'Rio de Janeiro', value: 'RJ' },
+  { label: 'Rio Grande do Norte', value: 'RN' },
+  { label: 'Rio Grande do Sul', value: 'RS' },
+  { label: 'Rondônia', value: 'RO' },
+  { label: 'Roraima', value: 'RR' },
+  { label: 'Santa Catarina', value: 'SC' },
+  { label: 'São Paulo', value: 'SP' },
+  { label: 'Sergipe', value: 'SE' },
+  { label: 'Tocantins', value: 'TO' },
+];
+
+
+const INSTRUMENTS = [
+	{ label: 'Vocal', value: 'singer' },
+	{ label: 'Violão', value: 'guitar' },
+	{ label: 'Guitarra', value: 'eletric_guitar' },
+	{ label: 'Contrabaixo', value: 'eletric_bass' },
+	{ label: 'Bateria', value: 'drums' },
+];
 
 export default class SignupForm extends Component {
   constructor(props) {
     super(props);
     this.state = {new_user: [],
-                  current_users: []};
-    this.data = ''
+                  current_users: [],
+                  selectedInstrumentsOption: '',
+                  selectedStateOption:'',
+                  };
 
     this.submitForm = this.submitForm.bind(this);
-    this.listUsers = this.listUsers.bind(this);
-
-
+    this.handleChange = this.handleChange.bind(this);
   }
 
   submitForm(event) {
@@ -21,59 +63,106 @@ export default class SignupForm extends Component {
 
     const data_user = {
           name: data_form.get('name'),
-          teamName: data_form.get('teamName'),
-          salary: data_form.get('salary')
+          instruments: data_form.get('instruments'),
     }
 
-    axios.post('http://localhost:8080/users/add', data_user)
-      .then(response => console.log(response.data))
+    console.log(data_user);
+
+    //
+    // axios.post('http://localhost:8080/users/add', data_user)
+    //   .then(response => console.log(response.data))
 
   }
 
-  listUsers(){
-    axios.get('http://localhost:8080/users/all')
-      .then(response => this.setState({current_users: response.data}))
-
-    var users = this.state.current_users.map(function(user){
-        return <tr key={user.id}>
-                <td>{user.id}</td>
-                <td>{user.name}</td>
-                <td>{user.teamName}</td>
-                <td>{user.salary}</td>
-              </tr>
-    });
-
-    return users;
+  handleChange = (type, selectedOption) => {
+    if (type == 'instruments') {
+        this.setState({selectedInstrumentsOption:selectedOption})
+    }
+    else if (type == 'states') {
+      this.setState({selectedStateOption:selectedOption})
+    }
   }
 
   render() {
+    const { selectedInstrumentsOption, selectedStateOption } = this.state;
+
     var html =
-        <div className="User">
-          <form className="User-form" onSubmit={this.submitForm}>
-            <label>Name:</label>
-            <input id="name" type="text" name="name"/><br/>
-            <label>Team:</label>
-            <input id="teamName" type="text" name="teamName"/><br/>
-            <label>Salary:</label>
-            <input id="salary" type="text" name="salary"/><br/>
+      <div className="container">
 
-            <input type="submit" value="Register" />
-          </form>
+        <h1 className="page-header"> Signup </h1>
 
-          <table className="App-table" align="center">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Team Name</th>
-                <th>Salary</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.listUsers()}
-            </tbody>
-          </table>
-        </div>
+        <form className="form-container" onSubmit={this.submitForm}>
+
+          <div className="form-group">
+            <label>Nome:</label>
+            <input type="text" className="form-control" name="name"
+                   id="name" placeholder="Insira seu nome"/>
+          </div>
+
+          <div className="form-group">
+            <label>E-mail:</label>
+            <input type="text" className="form-control" name="email"
+                   id="email" placeholder="Insira seu e-mail"/>
+          </div>
+
+          <div className="form-group">
+            <label>Data de nascimento:</label>
+            <InputMask className="form-control" name="birthdate"
+                       id="birthdate" placeholder="  /  /    "
+                       mask="99/99/9999" maskChar=" "/>
+          </div>
+
+          <div className="form-group">
+            <label>Instrumentos:</label>
+            <Select
+              name="instruments"
+              multi
+              placeholder="Selecione seus instrumentos"
+              value={selectedInstrumentsOption}
+              onChange={(value) => this.handleChange('instruments',value)}
+              options={INSTRUMENTS}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Estado:</label>
+            <Select
+              name="state"
+              placeholder="Selecioe seu Estado"
+              value={selectedStateOption}
+              onChange={(value) => this.handleChange('states',value)}
+              options={STATES}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Cidade:</label>
+            <input type="text" className="form-control" name="city" id="city" placeholder="Insira sua cidade"/>
+          </div>
+
+          <div className="form-group">
+            <label>Username:</label>
+            <input type="text" className="form-control" name="username" id="username" placeholder="Escolha um username"/>
+          </div>
+
+          <div className="form-group">
+            <label>Senha:</label>
+          <input type="text" className="form-control" name="password" id="password" placeholder="Insira uma senha"/>
+          </div>
+
+          <div className="form-group">
+            <label>Confirme a senha:</label>
+            <input type="password" className="form-control" name="repeat-password" id="repeat-password" placeholder="Sua senha novamente"/>
+          </div>
+
+
+          <div className="form-group" className="submit-btn">
+            <button type="submit" className="btn btn-primary">Cadastrar</button>
+          </div>
+
+        </form>
+      </div>
+
     return html;
   }
 }
--- pan-pan-frontend/src/index.js
+++ pan-pan-frontend/src/index.js
@@ -4,13 +4,13 @@ import { Route, BrowserRouter, Link} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 //Insert here the new pages
-import SignupForm from './components/User/signup/SignupForm';
-import HomePage from './pages/HomePage/HomePage';
+import SignupPage from './pages/SignupPage';
+import HomePage from './pages/HomePage';
 
 const Routes = () =>(
   <div>
     <Route exact path="/" component={HomePage} />
-    <Route path="/user/signup" component={SignupForm} />
+    <Route path="/user/signup" component={SignupPage} />
   </div>
 )
 
