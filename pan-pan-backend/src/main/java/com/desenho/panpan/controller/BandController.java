b3400855052c9a44eeab56930276ac7484557b49
Fabíola Fleury <fabiola.mfleury@gmail.com>
2018-04-21 14:09:35 -0300

    Add validations, getters and setters and band controller

--- pan-pan-backend/src/main/java/com/desenho/panpan/controller/BandController.java
+++ pan-pan-backend/src/main/java/com/desenho/panpan/controller/BandController.java
@@ -1,5 +1,33 @@
 package panpan.controller;
 
-public class BandController {
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.desenho.panpan.repository.BandRepository;
+import com.desenho.panpan.model.Band;
+import com.desenho.panpan.exception.InvalidRequestException;
+
+@RestController
+@RequestMapping(value="/band")
+public class BandController {
+  @Autowired
+  private BandRepository bandRepository;
+
+  @PostMapping(value = "/create")
+  public String add(@RequestBody @Valid final Band band, BindingResult bindingResult){
+    if (!bindingResult.hasErrors()){
+      bandRepository.save(band);
+      return "Done\n";
+    }else{
+      throw new InvalidRequestException("Invalid band", bindingResult);
+    }
+
+  }
 }
--- pan-pan-backend/src/main/java/com/desenho/panpan/model/Band.java
+++ pan-pan-backend/src/main/java/com/desenho/panpan/model/Band.java
@@ -1,6 +1,7 @@
 package com.desenho.panpan.model;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -17,21 +18,75 @@ public class Band {
   @GeneratedValue
   private int id;
 
+  @NotNull
   @OneToMany(mappedBy = "band")
   private List<User> members;
 
+  @NotNull
+  @Size(min=2, max=50)
   private String genre;
 
+  @NotNull
   private Date creationDate;
+
+  @NotNull
+  @Size(min=2, max=50)
   private String name;
 
+  @NotNull
+  @Size(min=2, max=50)
   private String city;
 
   public Band(String name){
-    this.name = name;
+    this.members = new ArrayList<User>();
+    setName(name);
   }
 
+  public int getId() {
+    return this.id;
+  }
+
+  public String getCity() {
+    return this.city;
+  }
+
+  public void setCity(String city){
+    this.city = city;
+  }
+
+  public void setGenre(String genre) {
+    this.genre = genre;
+  }
+
+  public String getGenre() {
+    return this.genre;
+  }
+
+  public void setCreationDate(Date creationDate) {
+      this.creationDate = creationDate;
+  }
+
+  public void setMembers(List<User> members) {
+    this.members = members;
+  }
+
+  public List<User> getMembers() {
+    return this.members;
+  }
+
+  public Date getCreationDate() {
+    return this.creationDate;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
   public String getName(){
     return this.name;
   }
+
+  public void addMember(User member) {
+    this.members.add(member);
+  }
+
 }
