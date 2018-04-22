95a080b6e165594d429b82ece7b4784c12c24eb2
shammyz <rodrigo.redcode@gmail.com>
2018-04-22 15:37:09 -0300

    Fix import error and add basic css for component

--- pan-pan-frontend/src/components/Band/create/CreateBandForm.jsx
+++ pan-pan-frontend/src/components/Band/create/CreateBandForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './CreateBandForm.css'
+import axios from 'axios'
 
 export default class CreateBandForm extends Component {
     constructor(props) {
@@ -13,13 +14,26 @@ export default class CreateBandForm extends Component {
     }
 
     handleSubmit(event){
-        const formData = new FormData(event.target);
-        const bandData = {
-            bandName: formData.get('band-name'),
-            bandStyle: formData.get('band-style'),
-            image: this.state.file,                       
+	const formData = new FormData(event.target);
+        const data = {
+            name: formData.get('band-name'),
+            genre: formData.get('band-style'),
+	    members: [],
+	    creationDate: new Date(),
+            //image: this.state.file,                       
         }
         /* send json to the API*/
+	const header = 'Access-Control-Allow-Origin'
+        axios.post('http://localhost:8080/api/bands/', data, header)
+        .then(response =>{
+            alert("CADASTROU")
+        }).catch(function(error){
+            if(error.response){
+                console.log(error.status)
+                console.log(error.data)
+                console.log(error.data)
+            }
+        })
     }
 
     handleFileChange(event){
@@ -28,27 +42,20 @@ export default class CreateBandForm extends Component {
 
     render() {
         return (
-        <div>
-            <div id="form-div">
-                <form id="band-form" onSubmit={this.handleSubmit}>
-                    <div id="band-name-section">
-                        <div className="label-section">
-                            <label>Nome:</label>
-                        </div>
-                        <div className="input-section">
-                            <input type="text" name="band-name" required="required"/>
-                        </div>
+        <div className="container">
+            <h1 className="page-header">Cadastre sua banda</h1>
+                <form id="form-container" onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label>Nome:</label>
+                        <input type="text" name="band-name" required="required" placeholder="Sua banda"/>
                     </div>
-                    <div id="band-style-section">
-                        <div className="label-section">
-                            <label>Estilo:</label>
-                        </div>
-                        <div className="input-section">
-                            <input type="text" name="band-style" required="required"/>
-                        </div>
+                    <div className="form-group">
+                        <label>Estilo:</label>
+                        <input type="text" name="band-style" required="required" placeholder="Estilo musical"/>
                     </div>
                     <div id="buttons-section">
                         <div className="single-btn-section">
+                            <label>Imagem:</label>
                             <input type="file" accept="image/*" onChange={this.handleFileChange}/>
                         </div>
                         <div className="single-btn-section">
@@ -57,7 +64,6 @@ export default class CreateBandForm extends Component {
                     </div>
                 </form>
             </div>
-        </div>
         )
     }
 }
