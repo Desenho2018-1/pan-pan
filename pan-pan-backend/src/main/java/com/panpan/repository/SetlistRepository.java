22df89cbe0d37c1cc2cda49fb8ac466f63bbb618
Roger Lenke <rogerlenke@gmail.com>
2018-04-25 18:50:19 -0300

    Create repository for setlist

---
+++ pan-pan-backend/src/main/java/com/panpan/repository/SetlistRepository.java
@@ -0,0 +1,9 @@
+package com.panpan.repository;
+
+import com.panpan.model.Setlist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SetlistRepository extends JpaRepository<Setlist, Long> {
+}
+
