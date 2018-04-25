cf6205f6d49e101a1667fcdf55d99ba7886e109b
Hugo Carvalho <hugonvsc@gmail.com>
2018-04-25 02:14:49 -0300

    Add reducers for signup

--- pan-pan-frontend/src/index.js
+++ pan-pan-frontend/src/index.js
@@ -3,24 +3,29 @@ import ReactDOM from 'react-dom';
 import { Route, BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 import routes from './routes.js'
 
-const store = createStore(
-  (state = {}) => state,
-  applyMiddleware(thunk)
+import rootReducer from './rootReducer'
 
-)
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(thunk),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
+);
+
 
 const Router = () => (
-  <BrowserRouter>
-    {routes}
-  </BrowserRouter>
+    <BrowserRouter>
+        {routes}
+    </BrowserRouter>
 );
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router />
-  </Provider>, document.getElementById('content'));
+    <Provider store={store}>
+        <Router />
+    </Provider>, document.getElementById('content'));
 registerServiceWorker();
---
+++ pan-pan-frontend/src/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+import flashMessages from './reducers/FlashMessages';
+
+
+export default combineReducers({
+    flashMessages
+});
