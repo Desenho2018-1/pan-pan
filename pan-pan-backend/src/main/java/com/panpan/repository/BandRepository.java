2318f112a4f8d27329eb95d059d267d56b024bfd
Josue Nascimento <josuetk63@gmail.com>
2018-04-25 21:53:28 -0300

    Create repository for band

--- pan-pan-backend/src/main/java/com/panpan/repository/BandRepository.java
+++ pan-pan-backend/src/main/java/com/panpan/repository/BandRepository.java
@@ -2,8 +2,10 @@ package com.panpan.repository;
 
 import com.panpan.model.Band;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public interface BandRepository extends JpaRepository<Band, Long>{
 
+  Band findBandById(long id);
 }
