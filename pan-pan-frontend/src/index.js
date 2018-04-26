dcbeeaee8e2556c5f395dbf0a8835aad4d7bbc5c
rodrigocam <rodrigo.redcode@gmail.com>
2018-04-25 23:43:32 -0300

    Add basic frontend for create a rehearsal

--- pan-pan-frontend/src/index.js
+++ pan-pan-frontend/src/index.js
@@ -7,12 +7,14 @@ import registerServiceWorker from './registerServiceWorker';
 import CreateBandPage from './pages/CreateBandPage';
 import SignupPage from './pages/SignupPage';
 import HomePage from './pages/HomePage';
+import CreateRehearsalPage from './pages/CreateRehearsalPage';
 
 const Routes = () =>(
   <div>
     <Route exact path="/" component={HomePage} />
     <Route exact path="/band/create" component={CreateBandPage}/>
     <Route path="/user/signup" component={SignupPage} />
+    <Route exact path="/rehearsal/create" component={CreateRehearsalPage}/>
   </div>
 )
 
