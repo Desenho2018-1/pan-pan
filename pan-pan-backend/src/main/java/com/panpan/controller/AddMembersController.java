8e92b81707dde45e7fa20b5356b64f8162ab01cd
Josue Nascimento <josuetk63@gmail.com>
2018-04-26 19:25:40 -0300

    Rename Class name

--- pan-pan-backend/src/main/java/com/panpan/controller/AddMembersController.java
+++ pan-pan-backend/src/main/java/com/panpan/controller/AddMembersController.java
@@ -6,23 +6,24 @@ import javax.persistence.PostRemove;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.panpan.model.Band;
-import com.panpan.repository.BandRepository;
 import com.panpan.repository.VerificationTokenRepository;
 
 import antlr.collections.List;
 
+@RequestMapping("/addMember")
 @RestController
-public class AddMembersControler {
+public class AddMembersController {
 
 	@Autowired
 	private BandRepository bandRepository;
 
 	@GetMapping("/idband")
 	public Band findBand(long idBand) {
-		List bandList = (List) bandRepository.findBandById(3);
-		return band;
+		
+		return null;
 	}
 }
