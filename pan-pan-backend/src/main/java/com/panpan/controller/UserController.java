753019400fcd787f15bdba53453b5cced768b015
Josue Nascimento <josuetk63@gmail.com>
2018-04-27 16:24:34 -0300

    Allow CORS

--- pan-pan-backend/src/main/java/com/panpan/controller/UserController.java
+++ pan-pan-backend/src/main/java/com/panpan/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.panpan.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,7 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 
-
+    @CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping(value = "/all")
 	public List <User> findAll() {
 	    return userRepository.findAll();
