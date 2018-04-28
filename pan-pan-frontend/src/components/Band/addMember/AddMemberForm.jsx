37252c4949925c11fd877cda07d8619c22b98403
Josue Nascimento <josuetk63@gmail.com>
2018-04-28 13:28:22 -0300

    Create set search member name

--- pan-pan-frontend/src/components/Band/addMember/AddMemberForm.jsx
+++ pan-pan-frontend/src/components/Band/addMember/AddMemberForm.jsx
@@ -5,14 +5,30 @@ import axios from "axios";
 export default class AddMemberForm extends Component{
   constructor(props){
     super(props);
-      this.state = {currentUsers:[]}
+      this.state = {
+        nameUser: '',
+        currentUsers:[]
+      }
 
-      this.listUsers = this.listUsers.bind(this);
       this.submitForm = this.submitForm.bind(this);
+      this.getUserName = this.getUserName.bind(this);
+      this.listUsers = this.listUsers.bind(this);
+      this.setUserName = this.setUserName.bind(this);
     }
     submitForm(event) {
       event.preventDefault();
-      this.listUsers()
+      this.getUserName();
+    }
+
+    setUserName(event){
+      this.setState({nameUser: event.target.value})
+    }
+
+    getUserName(){
+      console.log(this.state.nameUser);
+      axios.get('http://localhost:8080/user/'+this.state.nameUser)
+      .then(response=>console.log(response.data));
+
     }
 
     listUsers(){
@@ -30,35 +46,21 @@ export default class AddMemberForm extends Component{
             )
     });
     return users;
+
   }
 
-
-
   render (){
-    var hetml =
-    <div className="container">
-        <h1 className="page-title">Procure o Membro para adicionar a banda</h1>
-        <form id="form-container" onSubmit={this.submitForm}>
-            <div className="form-group">
-                <label>Nome:</label>
-                <input type="text" className="form-control" name="band-name" required="required" placeholder="Sua banda"/>
-            </div>
-            <input type="submit" value="Procura" />
-        </form>
-        <table className="App-table" align="center">
-        <thead>
-             <tr>
-               <th>ID</th>
-               <th>Name</th>
-               <th>Team Name</th>
-               <th>Salary</th>
-             </tr>
-      </thead>
-      <tbody>
-        {this.listUsers()}
-      </tbody>
-    </table>
-      </div>
-    return hetml;
+    var html =
+              <div className="container">
+                  <h1 className="page-title">Procure o Membro para adicionar a banda</h1>
+                  <form id="form-container" onSubmit={this.submitForm}>
+                      <div className="form-group">
+                          <label>Nome:</label>
+                          <input  value={this.state.nameUser}  onChange={this.setUserName}  type="text" className="form-control" name="band-name" required="required" placeholder="Sua banda"/>
+                      </div>
+                      <input type="submit" value="Procura" />
+                  </form>
+              </div>
+    return html;
   }
 }
