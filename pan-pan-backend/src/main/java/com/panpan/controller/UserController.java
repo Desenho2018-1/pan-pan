997262b9683450b7e3bf200301c25277a5155369
Josue Nascimento <josuetk63@gmail.com>
2018-04-28 13:51:22 -0300

    Get user name from api

--- pan-pan-backend/src/main/java/com/panpan/controller/UserController.java
+++ pan-pan-backend/src/main/java/com/panpan/controller/UserController.java
@@ -3,12 +3,14 @@ package com.panpan.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.panpan.model.User;
 import com.panpan.repository.UserRepository;
+
 
 import java.util.List;
 
@@ -20,9 +22,10 @@ public class UserController {
 	private UserRepository userRepository;
 
     @CrossOrigin(origins = "http://localhost:3000")
-	@GetMapping(value = "/all")
-	public List <User> findAll() {
-	    return userRepository.findAll();
+	@GetMapping(value = "/{name}")
+	public User findNameUser(@PathVariable String name) {
+    	System.out.println("entrei aqui");
+    	System.out.println("o nome que deu foi esse:"+name);
+	    return userRepository.findByFirstName(name);
 	}
-
-}
+ }
