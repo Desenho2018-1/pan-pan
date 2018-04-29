f35c5dbf1b7b6a90009a3598ab1195eb29294ebb
pablodiegoss <pablodiegoss@hotmail.com>
2018-04-29 13:33:57 -0300

    Rename Classes for Notification

--- pan-pan-backend/src/main/java/com/panpan/model/Notification.java
+++ pan-pan-backend/src/main/java/com/panpan/model/Notification.java
@@ -1,22 +1,39 @@
 package com.panpan.model;
 
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
-import com.panpan.Observer;
+import lombok.Data;
 
-public abstract class Notification {
-	List<Observer> observers;
-	public abstract String getLongText();
-	public abstract String getShortText();
-	public void addObserver(Observer o) {
-		observers.add(o);
+@Data
+@Entity
+public class EmailNotification extends Notification {
+	@ManyToOne
+    @JoinColumn(name = "user_id")
+	private User targetUser;
+	private boolean status = false;
+	private @Id @GeneratedValue Long notification_id;
+	private String longText;
+	private String shortText;
+	public EmailNotification() {
+	
+	};
+	public EmailNotification(User u, String longText, String shortText) {
+		this.setTargetUser(u);
+		this.setLongText(longText);
+		this.setShortText(shortText);
 	}
-	public void removeObserver(Observer o) {
-		observers.remove(o);
+	@Override
+	public String getLongText() {
+		return longText;
 	}
-	public void notifyObservers() {
-		for(Observer o:observers) {
-			o.update(this);
-		}
+	
+	@Override
+	public String getShortText() {
+		return shortText;
 	}
+
 }
