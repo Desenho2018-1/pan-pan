1e63fcfd242e7b2aeb76aaada457bfc446fa3af2
pablodiegoss <pablodiegoss@hotmail.com>
2018-04-29 17:35:47 -0300

    Refactor Notification

--- pan-pan-backend/src/main/java/com/panpan/model/Notification.java
+++ pan-pan-backend/src/main/java/com/panpan/model/Notification.java
@@ -1,39 +1,69 @@
 package com.panpan.model;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Transient;
+import javax.validation.constraints.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
 import lombok.Data;
+
+import com.panpan.model.User;
+import com.panpan.repository.NotificationRepository;
 
 @Data
 @Entity
-public class EmailNotification extends Notification {
+public class Notification implements Cloneable {
+	@Id
+	@GeneratedValue
+	private Long id;
 	@ManyToOne
-    @JoinColumn(name = "user_id")
-	private User targetUser;
-	private boolean status = false;
-	private @Id @GeneratedValue Long notification_id;
-	private String longText;
+	@JoinColumn(name = "creator_id")
+	private User creator;
+	@ManyToOne
+	@JoinColumn(name = "owner_id")
+	private User owner;
+
+	@ManyToOne
+	@JoinColumn(name = "band_activity_id")
+	private BandActivity band;
+
+	private Boolean isRead;
+	private String url;
 	private String shortText;
-	public EmailNotification() {
 	
-	};
-	public EmailNotification(User u, String longText, String shortText) {
-		this.setTargetUser(u);
-		this.setLongText(longText);
+	public Notification() {
+	}
+	@Autowired
+	public Notification(BandActivity band, User creator, String shortText, String url) {
+		this.setCreator(creator);
+		this.setBand(band);
 		this.setShortText(shortText);
+		this.setIsRead(false);
+		this.setUrl(url);
 	}
+
 	@Override
-	public String getLongText() {
-		return longText;
+	public Notification clone() {
+		Notification clone = null;
+		try {
+			clone = (Notification) super.clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		return clone;
 	}
 	
-	@Override
-	public String getShortText() {
-		return shortText;
-	}
 
 }
