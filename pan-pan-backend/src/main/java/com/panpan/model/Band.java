317966e2f2f4bf62ba4007d6f90be3e8e6814b0c
Fabíola Fleury <fabiola.mfleury@gmail.com>
2018-04-30 16:15:28 -0300

    Create song composite

--- pan-pan-backend/src/main/java/com/panpan/model/Band.java
+++ pan-pan-backend/src/main/java/com/panpan/model/Band.java
@@ -7,6 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
+import javax.persistence.CascadeType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.validation.constraints.*;
 import java.util.Date;
 import lombok.Data;
@@ -18,7 +22,7 @@ import com.panpan.model.User;
 public class Band {
     @Id
     @GeneratedValue
-    private Long id;
+    private Long bandId;
 
     @NotNull
     @OneToMany(mappedBy = "band")
@@ -34,6 +38,13 @@ public class Band {
     @NotNull
     @Size(min=2, max=50)
     private String name;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "band_music",
+      joinColumns = @JoinColumn(name = "song_id", referencedColumnName = "songId"),
+      inverseJoinColumns = @JoinColumn(name = "band_id",
+      referencedColumnName = "bandId"))
+    private List<SongComponent> songs;
 
     public Band(){}
 
