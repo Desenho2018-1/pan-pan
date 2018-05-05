b5975d5c83921d2a8d370432dff2e5f4d1dccbb5
pablodiegoss <pablodiegoss@hotmail.com>
2018-05-05 20:01:38 -0300

    Change BandRepository anotation

--- pan-pan-backend/src/main/java/com/panpan/repository/BandRepository.java
+++ pan-pan-backend/src/main/java/com/panpan/repository/BandRepository.java
@@ -2,9 +2,9 @@ package com.panpan.repository;
 
 import com.panpan.model.Band;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
-@Component
+@RepositoryRestController
 public interface BandRepository extends JpaRepository<Band, Long>{
 
   Band findBandById(long id);
