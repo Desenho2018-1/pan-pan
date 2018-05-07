6cd98a0b8f8195d43b15fa0612c02e51bd2f645d
pablodiegoss <pablodiegoss@hotmail.com>
2018-05-06 21:49:57 -0300

    Fix Test Structure

--- pan-pan-backend/src/test/java/com/panpan/PanPanApplicationTests.java
+++ pan-pan-backend/src/test/java/com/panpan/PanPanApplicationTests.java
@@ -1,57 +1,10 @@
 package com.panpan;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import java.util.Calendar;
-import static org.junit.Assert.assertEquals;
 
-
-import com.panpan.PanPanApplication;
-import com.panpan.model.User;
-import com.panpan.model.VerificationToken;
-import com.panpan.repository.UserRepository;
-import com.panpan.repository.VerificationTokenRepository;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes=PanPanApplication.class)
 public class PanPanApplicationTests {
-	  @Autowired
-	  private UserRepository userRepository;
-
-		@Autowired
-		private VerificationTokenRepository verificationTokenRepository;
-
-    private static String USER_MAIL = "adoniranbarbosa@gmail.com";
-    private static String USER_FIRSTNAME = "Adoniran";
-		private static String USER_LASTNAME = "Barbosa";
-		
-    @Test
-    public void givenUserRepositoryWhenSaveAndRetreiveUserThenOK() {
-			  User user = new User(USER_FIRSTNAME, USER_MAIL, "password");
-			  Calendar cal = Calendar.getInstance();
-			  int daysToIncrement = -5;
-			  cal.add(Calendar.DATE, daysToIncrement);
-			  user.setBirthDate(cal.getTime());
-
-			  user.setLastName(USER_LASTNAME);
-			  user.setUserName( USER_FIRSTNAME);
-			  user.setState("Idk");
-			  user.setCity("Where palm trees grow");
-				user.setActive(true);
-
-        this.userRepository.save(user);
-				this.verificationTokenRepository.save(new VerificationToken(user));
-
-        User foundUser = userRepository.findByFirstName(user.getFirstName());
-        assertNotNull(foundUser);
-        assertEquals(user.getId(), foundUser.getId());
-		}
-
+	
 }
