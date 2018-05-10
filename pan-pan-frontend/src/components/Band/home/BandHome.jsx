4e42e4322409578bb7bbdd5a12f756fe4c609cf9
Roger Lenke <rogerlenke@gmail.com>
2018-05-09 21:18:12 -0300

    Correct Band Home coding style

--- pan-pan-frontend/src/components/Band/home/BandHome.jsx
+++ pan-pan-frontend/src/components/Band/home/BandHome.jsx
@@ -1,85 +1,90 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import './BandHome.css'
+import './BandHome.css';
 
-class BandHome extends Component {
+const BandHome = (props) => {
+  // const { user } = props.login;
+  const { band } = props;
 
-    render() {
-        const user = this.props.login.user;
-        const band = this.props.band
+  return (
+    <div className="container band-home">
+      <div className="band-container">
+        <div className="page-header">
+          <h1>{band.name}</h1>
+        </div>
+        <div className="elements">
 
-        return  <div className="container band-home">
-                    <div className="band-container">
-                        <div className="page-header">
-                            <h1>{band.name}</h1>
-                        </div>
-                        <div className="elements">
+          <div className="element">
+            <Link to="#">
+              <button type="submit">
+                <span className="fas fa-users" aria-hidden="true" />
+              </button>
+              <p className="btn-text">Membros</p>
+            </Link>
+          </div>
 
-                            <div className="element">
-                                <Link to="#">
-                                    <button type="submit">
-                                        <span className="fas fa-users" aria-hidden="true"></span>
-                                    </button>
-                                    <p className="btn-text">Membros</p>
-                                </Link>
-                            </div>
+          <div className="element">
+            <Link to="#">
+              <button type="submit">
+                <span className="glyphicon glyphicon-music" aria-hidden="true" />
+              </button>
+              <p className="btn-text">Músicas</p>
+            </Link>
+          </div>
 
-                            <div className="element">
-                                <Link to="#">
-                                    <button type="submit">
-                                        <span className="glyphicon glyphicon-music" aria-hidden="true"></span>
-                                    </button>
-                                    <p className="btn-text">Músicas</p>
-                                </Link>
-                            </div>
+          <div className="element">
+            <Link to="/setlist/create">
+              <button type="submit">
+                <span className="glyphicon glyphicon-list-alt" aria-hidden="true" />
+              </button>
+              <p className="btn-text">SetList</p>
+            </Link>
+          </div>
 
-                            <div className="element">
-                                <Link to="#">
-                                    <button type="submit">
-                                        <span className="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
-                                    </button>
-                                    <p className="btn-text">SetList</p>
-                                </Link>
-                            </div>
+          <div className="element">
+            <Link to="#">
+              <button type="submit">
+                <span className="glyphicon glyphicon-calendar" aria-hidden="true" />
+              </button>
+              <p className="btn-text">Ensaios</p>
+            </Link>
+          </div>
 
-                            <div className="element">
-                                <Link to="#">
-                                    <button type="submit">
-                                        <span className="glyphicon glyphicon-calendar" aria-hidden="true"></span>
-                                    </button>
-                                    <p className="btn-text">Ensaios</p>
-                                </Link>
-                            </div>
+          <div className="element">
+            <Link to="#">
+              <button type="submit">
+                <span className="glyphicon glyphicon-folder-open" aria-hidden="true" />
+              </button>
+              <p className="btn-text">Docs</p>
+            </Link>
+          </div>
 
-                            <div className="element">
-                                <Link to="#">
-                                    <button type="submit">
-                                        <span className="glyphicon glyphicon-folder-open" aria-hidden="true"></span>
-                                    </button>
-                                    <p className="btn-text">Docs</p>
-                                </Link>
-                            </div>
+          <div className="element">
+            <Link to="#">
+              <button type="submit">
+                <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true" />
+              </button>
+              <p className="btn-text">Notificações</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-                            <div className="element">
-                                <Link to="#">
-                                    <button type="submit">
-                                        <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-                                    </button>
-                                    <p className="btn-text">Notificações</p>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-    }
-}
+BandHome.propTypes = {
+  // login: PropTypes.shape({
+  //   user: PropTypes.shape({}).isRequired,
+  // }).isRequired,
+  band: PropTypes.shape({}).isRequired,
+};
 
-function mapStateToProps(state) {
-    return {
-        login: state.login
-    }
-}
+const mapStateToProps = state => ({
+  login: state.login,
+});
 
-export default connect(mapStateToProps, { })(BandHome);
+export default connect(mapStateToProps, {})(BandHome);
